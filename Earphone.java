00e9bb82c4cb6c280cee0a5f110c9fb895b26101
YogeshTS007 <yogesh.xworkz@gmail.com>
2022-06-21 10:09:50 +0530

    commiting the earphones file

---
+++ Earphone.java
@@ -0,0 +1,16 @@
+class EarPhone{
+
+	public static void main (String a[]){
+		String Brand = "Jbl";
+		String colour = "Aqua green";
+		int Price = 2399;
+		
+		System.out.println("HeadPhone brand "+ Brand);
+		System.out.println("HeadPhone colour "+ colour);
+		System.out.println("HeadPhone Price "+ Price);
+	
+	}
+
+
+
+}
