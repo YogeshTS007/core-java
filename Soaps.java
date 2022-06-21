50e6d7b8fd01e6506d745325dc2ed30a8c486f81
YogeshTS007 <yogesh.xworkz@gmail.com>
2022-06-21 14:30:01 +0530

    commiting array examples

---
+++ Soaps.java
@@ -0,0 +1,10 @@
+class Soaps {
+
+	static String soaps[] = {"lux","lifebouy","dettol","rexona","pears","fiama","cinthol","mysore sandal","khadi natural","lever ayush"};
+
+	public static void main (String a[]) {
+
+		System.out.println(soaps [0] + "  "+ soaps [1]+ "    "+soaps[2]+ "    "+ soaps[3]+"    "+ soaps[4]+"    "+soaps [5]+"   "+soaps [6]+"    "+ soaps[7]+"   "+soaps[8]+"    "+ soaps[9]);
+	}
+
+}
---
+++ ToothPaste.java
@@ -0,0 +1,10 @@
+class ToothPaste {
+
+	static String toothPaste[] = {"colgate","pepsodent","close up","himalaya","sensodyne","patanjali","oral b","vicco vajradanthi","dabur meswak","dabur red"};
+
+	public static void main (String a[]) {
+
+		System.out.println(toothPaste [0] + "  "+ toothPaste [1]+ "    "+toothPaste[2]+ "    "+ toothPaste[3]+"    "+ toothPaste[4]+"    "+toothPaste [5]+"   "+toothPaste [6]+"    "+ toothPaste[7]+"   "+toothPaste[8]+"    "+ toothPaste[9]);
+	}
+
+}
